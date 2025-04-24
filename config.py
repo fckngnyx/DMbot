bd34e6f37d2b46c317f6b2dfe22681e61d36a574
Dmitry <fckngnyx@mail.ru>
2025-04-25 01:58:55 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+# Токен для админского бота
+ADMIN_BOT_TOKEN = os.getenv("ADMIN_BOT_TOKEN")
 
-TOKEN = os.getenv("TOKEN")
+# Токен для клиентского бота
 CLIENT_BOT_TOKEN = os.getenv("CLIENT_BOT_TOKEN")
+
 ADMIN_IDS = os.getenv("ADMIN_IDS", "").split(",")
